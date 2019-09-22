678e0ef2f606266d3130ebbbc87474d38ec0ccfe
Dongmin-Team <ehdals9687@gmail.com>
2019-09-22 23:03:36 +0900

    aaa

--- Seminar/src/Main.java
+++ Seminar/src/Main.java
@@ -5,7 +5,7 @@ public class Main {
 		int p = 23;
 		for (int x = 0; x < p; x++) {
 			int temp = ((int) Math.pow(x, 3) + x) % p;
-			System.out.println(temp);
+			System.out.println(temp);;
 		}
 	}
 
