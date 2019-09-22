210d340539eb457255f100c52ed5e6ff42cc2145
Dongmin-Team <ehdals9687@gmail.com>
2019-09-22 23:00:48 +0900

    1111

---
+++ Seminar/src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		int p = 23;
+		for (int x = 0; x < p; x++) {
+			int temp = ((int) Math.pow(x, 3) + x) % p;
+			System.out.println(temp);
+		}
+	}
+
+}
