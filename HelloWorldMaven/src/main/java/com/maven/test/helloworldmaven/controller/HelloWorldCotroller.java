036296699340bead2dbe727789793a6fad0ed3eb
Dongmin-Team <ehdals9687@gmail.com>
2019-09-26 21:15:43 +0900

    2115

--- HelloWorldMaven/src/main/java/com/maven/test/helloworldmaven/controller/HelloWorldCotroller.java
+++ HelloWorldMaven/src/main/java/com/maven/test/helloworldmaven/controller/HelloWorldCotroller.java
@@ -19,7 +19,7 @@ public class HelloWorldCotroller {
 	@RequestMapping("/hello")
 	public ModelAndView showMessage(
 	@RequestParam(value = "name", required = false, defaultValue = "World") String name) {
-		System.out.println("in controller.");
+		System.out.println("in controller");
 
 		ModelAndView mv = new ModelAndView("helloworld");
 		mv.addObject("message", message);
