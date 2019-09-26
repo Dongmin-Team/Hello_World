051dcb0289192fac9b4773a68df90acf575d46ba
Dongmin-Team <ehdals9687@gmail.com>
2019-09-26 23:27:30 +0900

    2327 TEst

--- HelloWorldMaven/src/main/java/com/maven/test/helloworldmaven/controller/HelloWorldCotroller.java
+++ HelloWorldMaven/src/main/java/com/maven/test/helloworldmaven/controller/HelloWorldCotroller.java
@@ -5,12 +5,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
-/**
- * 
- * @author Dongmin Kim.
- *
- */
-
 
 @Controller
 public class HelloWorldCotroller {
